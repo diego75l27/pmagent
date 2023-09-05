40c713f1fc4d4fc32b2355dc94d6b91cefa62dd2
Diego Banchero <diegobanchero@Diegos-MBP.homenet.telecomitalia.it>
2023-09-05 22:33:41 +0200

    Added new file and update pythia

--- pythia.py
+++ pythia.py
@@ -4,14 +4,39 @@ import pandas as pd
 import gradio as gr
 import os
 import neo4jCSVUploader
+import evmDBChat
 
 # load environment variables from .env.list
 load_dotenv()
 
+user = os.getenv("MARIADB_USER")
+password = os.getenv("MARIADB_PASS")
+host = os.getenv("MARIADB_HOST")
+port = os.getenv("MARIADB_PORT")
+database = os.getenv("MARIADB_DBASE")
+
+open_ai_key = os.getenv("OPENAI_API_KEY")
+gpt_model = os.getenv("model-gpt3")
+temperature = float(os.getenv("temperature"))
+dburl = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
+
+config_dict = {
+    "user": user,
+    "password": password,
+    "host": host,
+    "port": port,
+    "database": database,
+    "open_ai_key": open_ai_key,
+    "gpt_model": gpt_model,
+    "temperature": temperature,
+    "dburl": dburl
+}
+
 
 # Load chat interface
 def pythiaChat(message, history):
-    return random.choice(["Yes", "No"])
+    evmChat = evmDBChat.evmDBChat(config_dict)
+    return evmChat.chatEvmDb(message)
 
 
 # Neo4j configuration
@@ -19,13 +44,11 @@ uri = os.getenv('GRAPHDB_URI')
 username = os.getenv('GRAPHDB_USER')
 password = os.getenv('GRAPHDB_PASS')
 
-
 # Instantiate the Neo4jCSVUploader class
 uploader = neo4jCSVUploader.Neo4jCSVUploader(uri, username, password)
 
-
 with gr.Blocks() as iface:
-    with gr.Tab("Upload Nodes"):
+    with gr.Tab("Upload Nodes & Relationships"):
         inp = gr.File()
         uploadNodes = gr.Button(value="Submit")
         out = gr.Textbox()
@@ -34,8 +57,11 @@ with gr.Blocks() as iface:
             inputs=inp,
             outputs=out
         )
-    with gr.Tab("Upload Relationship"):
-        gr.Button("New Tiger")
-
+    with gr.Tab("Q&A SQL Data"):
+        gr.ChatInterface(
+            fn=pythiaChat,
+            title="EVM Agent",
+            description="Your Project Management Agent for Enhanced Productivity",
+        )
 
 iface.launch()
