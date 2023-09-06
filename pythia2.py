b83453051e371d42805002f8fe2a346277753a86
Diego Banchero <diegobanchero@Diegos-MBP.homenet.telecomitalia.it>
2023-09-06 16:39:13 +0200

    fixed

--- pythia2.py
+++ pythia2.py
@@ -47,4 +47,4 @@ with gr.Blocks() as iface:
             description="Your Project Management Agent for Enhanced Productivity",
         )
 
-iface.launch(share=True)
+iface.launch(share=True, server_name="0.0.0.0")
